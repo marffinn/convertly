d5d68c8e9e7f6df17678491a556f8df65fea5b08
marffinn <marffinn@gmail.com>
2025-08-27 16:42:41 +0200

    Logo lilnks to start page

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ThemeContext } from './ThemeContext';
 import './Header.css';
 import logo from './convertly.png';
@@ -6,13 +7,19 @@ console.log('Logo imported:', logo);
 
 function Header() {
   useContext(ThemeContext);
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
 
   return (
     <header className="app-header">
       <div className="container narrow-container">
         <div className="header-content">
-          <img src={logo} alt="Convertly Logo" className="logo" />
-          
+          <div onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
+            <img src={logo} alt="Convertly Logo" className="logo" />
+          </div>
         </div>
       </div>
     </header>
